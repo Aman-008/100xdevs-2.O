5555da764c65fddef2d8ea217735d375f760775e
Aman-008 <amanp9568@gmail.com>
2023-12-14 03:44:39 +0530

    commit

---
+++ Class 1/Assignments/reverse_element.js
@@ -0,0 +1,5 @@
+let num = [20, 30, 44, 33, 65, 86, 97, 99, 108];
+let revNum = num.length - 1;
+for (let i = revNum; i < num.length; i--) {
+    console.log(revNum[i]);
+}
---
+++ Class 1/function.js
@@ -0,0 +1,56 @@
+// function findSum(a, b) {
+//     // do things with the input and return an output
+//     const sumValue = a + b;
+//     return sumValue;
+// }
+// const value = findSum(2, 3);
+// console.log(value)
+
+
+// function sum(num1, num2, fnToCall) {
+//     let result = num1 + num2;
+//     fnToCall(result);
+// }
+
+// function displayResult(data) {
+//     console.log("Result of the sum is " + data);
+// }
+
+// function displayResultPassive(data) {
+//     console.log("Sum's result is : " + data);
+// }
+
+// const ans = sum(2, 3, displayResult);
+
+// // callbacks
+
+
+//function calculateArithmetic(a, b, type) {
+//    if (type == "sum") {
+//	return a + b;
+//    }
+//    if (type == "minus") {
+//	return a - b;
+//    }
+//}
+//const value = calculateArithmetic(7, 9, "minus");
+//console.log(value);
+
+
+//function getLength(str) {
+//    console.log("Original String : ", str)
+//    console.log("Length : ", str.length)
+//}
+//getLength("This is the man from toranto");
+
+
+
+/*
+function findIndexof(str, target) {
+    console.log("Original String : ", str);
+    console.log("Index : ", str.indexOf(target));
+}
+findIndexof("Namaste Duniya, Welcome to the world of coding", "of")
+*/
+
+
